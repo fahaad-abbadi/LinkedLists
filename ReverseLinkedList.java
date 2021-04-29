6dc594daf4fe2421a73301d173d3f7a2ff74c622
fahaad-abbadi <al.fahaadabbadi@gmail.com>
2021-04-29 20:28:37 +0530

    initial commit

--- ReverseLinkedList.java
+++ ReverseLinkedList.java
@@ -1,14 +1,4 @@
 // function to reverse a linkedlist
-class Node {
-    int data;
-    Node next;
-
-    Node() {
-        data = 0;
-        next = null;
-    }
-}
-
 class ReverseLinkedList {
     public static Node reverseList(Node head) {
         Node p = null, n = null;
---
+++ ReverseLinkedListRecursive.java
@@ -0,0 +1,13 @@
+class ReverseLinkedListRecursive {
+    static Node reverse(Node head) {
+        if (head == null || head.next == null)
+            return head;
+
+        Node temp = reverse(head.next);
+
+        head.next.next = head;
+        head.next = null;
+
+        return temp;
+    }
+}
